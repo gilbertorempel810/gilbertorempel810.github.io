49dbe4ad03a5ed1b5292afdd3f4daf5e72f0f479
gilbertorempel810 <79403220+gilbertorempel810@users.noreply.github.com>
2021-02-21 07:15:20 -0500

    Updates landing v0.02
    hello buddy

--- init.js
+++ init.js
@@ -1,30 +1,18 @@
-/*
-	Helios 1.0 by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
-/*********************************************************************************/
-/* Settings                                                                      */
-/*********************************************************************************/
 
 	var helios_settings = {
 
-		// Header (homepage only)
 			header: {
 				fullScreen: true,
 				fadeIn: true,
 				fadeDelay: 500
 			},
 
-		// Carousels
 			carousels: {
 				speed: 4,
 				fadeIn: true,
 				fadeDelay: 250
 			},
 
-		// Dropotron (dropdown menus)
 			dropotron: {
 				mode: 'fade',
 				speed: 350,
@@ -34,9 +22,8 @@
 				offsetY: -16
 			},
 
-		// skelJS (probably don't need to change anything here unless you know what you're doing)
 			skelJS: {
-				prefix: 'css/style',
+				prefix: 'style',
 				resetCSS: true,
 				boxModel: 'border',
 				grid: {
@@ -52,7 +39,6 @@
 				}
 			},
 
-		// skelJS Plugins (ditto; don't change unless you know what you're doing)
 			skelJSPlugins: {
 				panels: {
 					transformBreakpoints: 'mobile',
@@ -78,16 +64,8 @@
 
 	}
 
-/*********************************************************************************/
-/* Don't modify beyond this point unless you know what you're doing!             */
-/*********************************************************************************/
-
-// Initialize skelJS
 	skel.init(helios_settings.skelJS, helios_settings.skelJSPlugins);
 
-// jQuery functions
-
-	// scrolly
 		jQuery.fn.n33_scrolly = function() {				
 			jQuery(this).click(function(e) {
 				var h = jQuery(this).attr('href'), target;
@@ -101,7 +79,6 @@
 			});
 		};
 
-	// preloadImage
 		jQuery.n33_preloadImage = function(url, onload) {
 			var	$img = $('<img />'),
 				_IEVersion = (navigator.userAgent.match(/MSIE ([0-9]+)\./) ? parseInt(RegExp.$1) : 99);
@@ -115,13 +92,10 @@
 				$img.load(onload);
 		};
 
-	// formerize
 		jQuery.fn.n33_formerize=function(){var _fakes=new Array(),_form = jQuery(this);_form.find('input[type=text],textarea').each(function() { var e = jQuery(this); if (e.val() == '' || e.val() == e.attr('placeholder')) { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).blur(function() { var e = jQuery(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).focus(function() { var e = jQuery(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); _form.find('input[type=password]').each(function() { var e = jQuery(this); var x = jQuery(jQuery('<div>').append(e.clone()).remove().html().replace(/type="password"/i, 'type="text"').replace(/type=password/i, 'type=text')); if (e.attr('id') != '') x.attr('id', e.attr('id') + '_fakeformerizefield'); if (e.attr('name') != '') x.attr('name', e.attr('name') + '_fakeformerizefield'); x.addClass('formerize-placeholder').val(x.attr('placeholder')).insertAfter(e); if (e.val() == '') e.hide(); else x.hide(); e.blur(function(event) { event.preventDefault(); var e = jQuery(this); var x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } }); x.focus(function(event) { event.preventDefault(); var x = jQuery(this); var e = x.parent().find('input[name=' + x.attr('name').replace('_fakeformerizefield', '') + ']'); x.hide(); e.show().focus(); }); x.keypress(function(event) { event.preventDefault(); x.val(''); }); });  _form.submit(function() { jQuery(this).find('input[type=text],input[type=password],textarea').each(function(event) { var e = jQuery(this); if (e.attr('name').match(/_fakeformerizefield$/)) e.attr('name', ''); if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); }).bind("reset", function(event) { event.preventDefault(); jQuery(this).find('select').val(jQuery('option:first').val()); jQuery(this).find('input,textarea').each(function() { var e = jQuery(this); var x; e.removeClass('formerize-placeholder'); switch (this.type) { case 'submit': case 'reset': break; case 'password': e.val(e.attr('defaultValue')); x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } else { e.show(); x.hide(); } break; case 'checkbox': case 'radio': e.attr('checked', e.attr('defaultValue')); break; case 'text': case 'textarea': e.val(e.attr('defaultValue')); if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } break; default: e.val(e.attr('defaultValue')); break; } }); window.setTimeout(function() { for (x in _fakes) _fakes[x].trigger('formerize_sync'); }, 10); }); return _form; };
 
-	// onVisible
 		(function() {
 		
-			// Vars
 				var $window = jQuery(window),
 					elements = [],
 					delay = 10,
@@ -144,29 +118,24 @@
 						}
 					};
 
-			// Event bindings
 				$window.load(function() {
 
 					$window.on('scroll resize', function() {
 
-						// Clear existing timeout (if one exists)
 							window.clearTimeout(timerId);
 
-						// Set our poll function to run after (delay)ms (prevents it from running until the user's done scrolling/resizing)
 							timerId = window.setTimeout(function() { (poll)(); }, delay);
 
 					}).trigger('resize');
 
 				});
 
-			// onVisible jQuery function (pretty much just adds the element to our list of elements to poll)
 				jQuery.fn.n33_onVisible = function(fn, p) {
 					elements.push({ o: jQuery(this), fn: fn, pad: (p ? p : pad), state: false });
 				};
 
 		})();
 
-// Ready stuff
 	jQuery(function() {
 
 		var $window = $(window),
@@ -176,23 +145,18 @@
 			_isTouch = !!('ontouchstart' in window),
 			_isMobile = !!(navigator.userAgent.match(/(iPod|iPhone|iPad|Android|IEMobile)/));
 
-		// Pause CSS transitions until the page has loaded (prevents "flickering")
 			$body.addClass('paused');
 			$window.load(function() {
 				$body.removeClass('paused');
 			});
 
-		// Add input "placeholder" support to IE <= 9
 			if (_IEVersion < 10)
 				$('form').n33_formerize();
 
-		// Initialize scrolly links
 			$('.scrolly').n33_scrolly();
 
-		// Initialize dropotron
 			$('#nav > ul').dropotron(helios_settings.dropotron);
 
-		// Initialize carousels
 			$('.carousel').each(function() {
 				
 				var	$t = $(this),
@@ -208,7 +172,6 @@
 					reelWidth,
 					timerId;
 
-				// Items
 					if (helios_settings.carousels.fadeIn)
 					{
 						$items.addClass('loading');
@@ -241,7 +204,6 @@
 						}, 50);
 					}
 				
-				// Main
 					$t._update = function() {
 						pos = 0;
 						rightLimit = (-1 * reelWidth) + $window.width();
@@ -254,7 +216,6 @@
 					else
 						$t._updatePos = function() { $reel.css('transform', 'translate(' + pos + 'px, 0)'); };
 					
-				// Forward
 					$forward
 						.appendTo($t)
 						.hide()
@@ -275,7 +236,6 @@
 							window.clearInterval(timerId);
 						});
 				
-				// Backward	
 					$backward
 						.appendTo($t)
 						.hide()
@@ -296,7 +256,6 @@
 							window.clearInterval(timerId);
 						});
 						
-				// Init
 					$window.load(function() {
 
 						reelWidth = $reel[0].scrollWidth;
@@ -333,16 +292,13 @@
 				
 			});
 
-		// Initialize header
 
-			// Mobile devices don't do so well with fixed backgrounds and the fullscreen header :/
 				if (_isMobile)
 				{
 					$header.css('background-attachment', 'scroll');
 					helios_settings.header.fullScreen = false;
 				}
 
-			// Homepage header
 				if ($body.hasClass('homepage'))
 				{
 					if (helios_settings.header.fullScreen)
